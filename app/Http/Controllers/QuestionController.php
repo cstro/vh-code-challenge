5d8909f2b6ec8bf67c2a1c56a4104a459b55f27f
Chris Stroud <chris.stroud00@gmail.com>
2020-01-02 20:46:36 +0000

    Style up question index page
    Add styling to the form on the question index page. Also show all
    previously asked questions, displayed as links to the question show page.

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -14,7 +14,8 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        return view('questions.index');
+        $questions = Question::all();
+        return view('questions.index', ['questions' => $questions]);
     }
 
     /**
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -3,14 +3,40 @@
 @section('title', 'Questions')
 
 @section('content')
-    <h1>Questions</h1>
 
-    <form method="POST" action="{{ route('questions.store') }}">
-        {{ csrf_field() }}
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="display-4">Vegan Hacktavist - Q&A</h1>
+            <p class="lead">A place for you to ask any questions you have about Vegans!</p>
+        </div>
+    </div>
 
-        <label for="question">What's your question?</label>
-        <textarea id="question" name="question"></textarea>
+    <div class="row mb-4">
+        <div class="container">
+            <h2>Ask us a question below...</h2>
+            <form method="POST" action="{{ route('questions.store') }}">
+                {{ csrf_field() }}
 
-        <button>Ask question</button>
-    </form>
+                <div class="form-group">
+                    <label for="question">What would you like to know?</label>
+                    <textarea id="question" name="question" class="form-control"></textarea>
+                    <small class="form-text text-muted">Make sure you end you question with a "?"</small>
+                </div>
+
+                <button class="btn btn-primary float-right">Ask question</button>
+            </form>
+        </div>
+    </div>
+
+    <div class="container">
+        <h2 class="mb-4">...Or check out our previously asked questions</h2>
+        @foreach ($questions as $question)
+            <a href="{{ route('questions.show', [$question]) }}">
+                <h3>{{ $question->content }}</h3>
+            </a>
+
+            <hr class="my-3"/>
+        @endforeach
+    </div>
+
 @endsection
